c650ff6c82e81db6f26db2ed49d0403dd0444cb0
Murilo Lopes <mloopes11@gmail.com>
2023-05-22 23:24:50 -0300

    splash screen

---
+++ frontend/broly/src/app/splash.tsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useRef } from "react";
+import lottie, { AnimationItem } from "lottie-web";
+import animationData from "./animation.json";
+
+const SplashScreen: React.FC = () => {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const animRef = useRef<AnimationItem | null>(null);
+
+  useEffect(() => {
+    if (containerRef.current) {
+      animRef.current = lottie.loadAnimation({
+        container: containerRef.current,
+        renderer: "svg",
+        loop: true,
+        autoplay: true,
+        animationData: animationData,
+      });
+    }
+
+    return () => {
+      if (animRef.current) {
+        animRef.current.destroy();
+      }
+    };
+  }, []);
+
+  return (
+    <div
+      ref={containerRef}
+      style={{
+        width: "50vw",
+        height: "50vw",
+        position: "fixed",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+      }}
+    />
+  );
+};
+
+export default SplashScreen;
